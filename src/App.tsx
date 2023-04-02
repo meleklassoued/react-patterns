1690609dbae41b154e41b36b804f8d7e66fafcda
meleklassoued <lassouedm07@gmail.com>
2023-04-02 21:27:50 +0100

    feat: :sparkles: add some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
-function App() {
+function App(): JSX.Element {
+  // state Hook:
+  const [counter, setCounter] = useState<number>(0)
+  // handle counter:
+  const handleCounter = (operation: string): void => {
+    if (operation === 'add') {
+      return setCounter(counter + 1)
+    }
+    return setCounter(counter - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>counter:{counter}</p>
+      <button type="button" onClick={() => handleCounter('add')}>
+        Increment
+      </button>
+      <button type="button" onClick={() => handleCounter('dec')}>
+        Decrement
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
